d313e4ee4e2ed0fc26a205f14c304294317b603f
Selvia Melani Putri <selviamelaniputri06@gmail.com>
2026-09-19 18:53:41 +0800

    Latihan mandiri: KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author asus
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String[] args) {
+        String nama = "Selvia Melani Putri";
+        String npm  = "2410010140";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin terus memahami konsep OOP lebih baik "
+                + "agar bisa memahami cara kerja aplikasi nyata "
+                + "yang bukan hanya coding dasar saja";
+        
+        System.out.println("=========================================");
+        System.out.println("Nama        : " + nama);
+        System.out.println("NPM         : " + npm);
+        System.out.println("Prodi       : " + prodi);
+        System.out.println("Semester    : " + semester);
+        System.out.println("Alasan      : " + alasan);
+        System.out.println("=========================================");
+    }
+}
